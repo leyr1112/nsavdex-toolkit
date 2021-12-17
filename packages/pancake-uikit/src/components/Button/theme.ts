171bb09c5fd009e55e1f6cfc5ba21e0846702c18
BlockChain SuperDev <leyr1114@gmail.com>
2021-12-18 04:22:02 +1000

    commit

--- packages/pancake-uikit/src/components/Button/theme.ts
+++ packages/pancake-uikit/src/components/Button/theme.ts
@@ -19,7 +19,7 @@ export const scaleVariants = {
 export const styleVariants = {
   [variants.PRIMARY]: {
     backgroundColor: "primary",
-    color: "white",
+    color: "black",
   },
   [variants.SECONDARY]: {
     backgroundColor: "transparent",
