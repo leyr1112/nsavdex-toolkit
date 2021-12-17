72af58473ffbc43756a94f949c5f3a2940a1cf52
BlockChain SuperDev <leyr1114@gmail.com>
2021-12-18 00:37:55 +1000

    first commit

---
+++ packages/pancake-uikit/src/components/CakePrice/CakePrice.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import { PancakeRoundIcon } from "../Svg";
+import Text from "../Text/Text";
+import Skeleton from "../Skeleton/Skeleton";
+import { Colors } from "../../theme";
+
+export interface Props {
+  color?: keyof Colors;
+  cakePriceUsd?: number;
+}
+
+const PriceLink = styled.a`
+  display: flex;
+  align-items: center;
+  svg {
+    transition: transform 0.3s;
+  }
+  :hover {
+    svg {
+      transform: scale(1.2);
+    }
+  }
+`;
+
+const CakePrice: React.FC<Props> = ({ cakePriceUsd, color = "textSubtle" }) => {
+  return cakePriceUsd ? (
+    <PriceLink
+      href="https://swap.nsavdex.io/swap?outputCurrency=0x4837eB79E05C3aC845C010060C488992d1f1AcE0"
+      target="_blank"
+    >
+      <PancakeRoundIcon width="24px" mr="8px" />
+      <Text color={color} bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
+    </PriceLink>
+  ) : (
+    <Skeleton width={80} height={24} />
+  );
+};
+
+export default React.memo(CakePrice);
---
+++ packages/pancake-uikit/src/components/Footer/config.tsx
@@ -0,0 +1,158 @@
+import { Language } from "../LangSelector/types";
+import { FooterLinkType } from "./types";
+
+export const footerLinks: FooterLinkType[] = [
+  {
+    label: "About",
+    items: [
+      {
+        label: "Contact",
+        href: "https://docs.swap.nsavdex.io/contact-us",
+      },
+      {
+        label: "Blog",
+        href: "https://nsavdex.medium.com/",
+      },
+      {
+        label: "Community",
+        href: "https://docs.swap.nsavdex.io/contact-us/telegram",
+      },
+      {
+        label: "CAKE",
+        href: "https://docs.swap.nsavdex.io/tokenomics/cake",
+      },
+      {
+        label: "—",
+      },
+      {
+        label: "Online Store",
+        href: "https://nsavdex.creator-spring.com/",
+        isHighlighted: true,
+      },
+    ],
+  },
+  {
+    label: "Help",
+    items: [
+      {
+        label: "Customer",
+        href: "Support https://docs.swap.nsavdex.io/contact-us/customer-support",
+      },
+      {
+        label: "Troubleshooting",
+        href: "https://docs.swap.nsavdex.io/help/troubleshooting",
+      },
+      {
+        label: "Guides",
+        href: "https://docs.swap.nsavdex.io/get-started",
+      },
+    ],
+  },
+  {
+    label: "Developers",
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/nsavdex",
+      },
+      {
+        label: "Documentation",
+        href: "https://docs.swap.nsavdex.io",
+      },
+      {
+        label: "Bug Bounty",
+        href: "https://app.gitbook.com/@nsavdex-1/s/nsavdex/code/bug-bounty",
+      },
+      {
+        label: "Audits",
+        href: "https://docs.swap.nsavdex.io/help/faq#is-nsavdex-safe-has-nsavdex-been-audited",
+      },
+      {
+        label: "Careers",
+        href: "https://docs.swap.nsavdex.io/hiring/become-a-chef",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Twitter",
+    icon: "Twitter",
+    href: "https://twitter.com/nsavdex",
+  },
+  {
+    label: "Telegram",
+    icon: "Telegram",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/nsavdex",
+      },
+      {
+        label: "Bahasa Indonesia",
+        href: "https://t.me/nsavdexIndonesia",
+      },
+      {
+        label: "中文",
+        href: "https://t.me/nsavdex_CN",
+      },
+      {
+        label: "Tiếng Việt",
+        href: "https://t.me/nsavdexVN",
+      },
+      {
+        label: "Italiano",
+        href: "https://t.me/nsavdex_ita",
+      },
+      {
+        label: "русский",
+        href: "https://t.me/nsavdex_ru",
+      },
+      {
+        label: "Türkiye",
+        href: "https://t.me/nsavdexturkiye",
+      },
+      {
+        label: "Português",
+        href: "https://t.me/nsavdexPortuguese",
+      },
+      {
+        label: "Español",
+        href: "https://t.me/nsavdexEs",
+      },
+      {
+        label: "日本語",
+        href: "https://t.me/nsavdexjp",
+      },
+      {
+        label: "Français",
+        href: "https://t.me/nsavdexfr",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/nsavdexAnn",
+      },
+      {
+        label: "Whale Alert",
+        href: "https://t.me/nsavdexWhales",
+      },
+    ],
+  },
+  {
+    label: "Github",
+    icon: "Github",
+    href: "https://github.com/nsavdex/",
+  },
+  {
+    label: "Discord",
+    icon: "Discord",
+    href: "https://discord.gg/nsavdex",
+  },
+];
+
+export const langs: Language[] = [...Array(20)].map((_, i) => ({
+  code: `en${i}`,
+  language: `English${i}`,
+  locale: `Locale${i}`,
+}));
